ddae03d5869c85ceb652be755a848be06b708798
morteza <khodemousa@gmail.com>
2020-12-13 23:53:17 +0330

    template

---
+++ 02-template/template.js
@@ -0,0 +1,8 @@
+new Vue({
+   el:'#template',
+    data:{
+        firstname:'Morteza',
+        lastname:'Mousakhani',
+        htmlcontent:'<div class="text-danger">My first name is {{firstname}} and last name is {{lastname}}  </div>'
+    },
+});
